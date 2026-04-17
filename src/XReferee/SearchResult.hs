2da6fafe51a8b77667acf35b719ed4ad1e2558ca
Brandon Chinn <brandonchinn178@gmail.com>
2026-04-16 20:18:23 -0700

    Switch to lazy bytestring

--- src/XReferee/SearchResult.hs
+++ src/XReferee/SearchResult.hs
@@ -19,13 +19,15 @@ import Control.Applicative ((<|>))
 import Control.DeepSeq (NFData (..), ($!!))
 import Control.Exception (evaluate)
 import Control.Monad (guard, when)
+import Data.ByteString.Lazy (LazyByteString)
+import Data.ByteString.Lazy qualified as LBS
+import Data.ByteString.Lazy.Char8 qualified as LBS.Char8
 import Data.Map (Map)
 import Data.Map qualified as Map
 import Data.Maybe (fromMaybe)
 import Data.Text (Text)
 import Data.Text qualified as Text
-import Data.Text.Lazy qualified as TextL
-import Data.Text.Lazy.IO qualified as TextL
+import Data.Text.Encoding qualified as Text
 import System.Exit (ExitCode (..))
 import System.IO qualified as IO
 import System.Process qualified as Process
@@ -115,24 +117,25 @@ findRefsFromGit opts = do
           , Process.std_err = Process.CreatePipe
           }
   Process.withCreateProcess proc $ \_ stdoutHandle stderrHandle ph -> do
-    stdout <- maybe (pure "") TextL.hGetContents stdoutHandle
-    result <- evaluate $!! mconcat . map parseLine . TextL.lines $ stdout
+    stdout <- maybe (pure "") LBS.hGetContents stdoutHandle
+    result <- evaluate $!! mconcat . map parseLine . LBS.Char8.lines $ stdout
     code <- Process.waitForProcess ph
-    stderr <- maybe (pure "") TextL.hGetContents stderrHandle
-    TextL.hPutStr IO.stderr stderr
-    when (code /= ExitSuccess && (not . TextL.null) stderr) $
+    stderr <- maybe (pure "") LBS.hGetContents stderrHandle
+    LBS.hPutStr IO.stderr stderr
+    when (code /= ExitSuccess && (not . LBS.null) stderr) $
       -- TODO: Proper error?
       errorWithoutStackTrace "git grep failed"
     pure result
   where
     parseLine line = fromMaybe mempty $ do
-      [filepath, lineNumStr, colNumStr, rest] <- pure $ TextL.splitOn "\0" line
-      lineNum <- readMaybe $ TextL.unpack lineNumStr
-      colNum <- readMaybe $ TextL.unpack colNumStr
+      -- Split on \NUL characters
+      [filepath, lineNumStr, colNumStr, rest] <- pure $ LBS.split 0 line
+      lineNum <- readMaybe $ LBS.Char8.unpack lineNumStr
+      colNum <- readMaybe $ LBS.Char8.unpack colNumStr
       let (anchors, references) = parseLabels rest colNum
           mkLoc columnRange =
             LabelLoc
-              { filepath = TextL.unpack filepath
+              { filepath = LBS.Char8.unpack filepath
               , lineNum
               , columnRange
               }
@@ -145,7 +148,7 @@ findRefsFromGit opts = do
 -- | Parse all labels from the given text.
 parseLabels ::
   -- | The text to parse for labels.
-  TextL.Text ->
+  LazyByteString ->
   -- | The column number of the first label in the input text. Used to calculate the column numbers for the labels.
   Int ->
   ([(Anchor, ColumnRange)], [(Reference, ColumnRange)])
@@ -153,45 +156,64 @@ parseLabels text col =
   parseSomeMarker
     []
     []
-    (TextL.drop (fromIntegral col - 1) text)
+    (LBS.drop (fromIntegral col - 1) text)
     col
   where
-    markerStarts = map Text.head [anchorStart, refStart]
+    markerStarts = map (LBS.head . toLBS) [anchorStart, refStart]
+    toLBS = LBS.fromStrict . Text.encodeUtf8
+    toText = Text.decodeUtf8 . LBS.toStrict
 
     parseSomeMarker ::
       [(Anchor, ColumnRange)] ->
       [(Reference, ColumnRange)] ->
-      TextL.Text ->
+      LazyByteString ->
       Int ->
       ([(Anchor, ColumnRange)], [(Reference, ColumnRange)])
     parseSomeMarker anchors refs s0 col0 =
       -- Remove the prefix before the next marker, and update the column number accordingly.
-      let (prefix, s1) = TextL.break (`elem` markerStarts) s0
-          col1 = col0 + fromIntegral (TextL.length prefix)
+      let (prefix, s1) = LBS.break (`elem` markerStarts) s0
+          col1 = col0 + fromIntegral (LBS.length prefix)
        in case (Left <$> parseAnchor s1) <|> (Right <$> parseRef s1) of
             Just (Left (name, s2)) ->
-              let markerLen = Text.length anchorStart + fromIntegral (TextL.length name) + Text.length anchorEnd
+              let markerLen = Text.length anchorStart + fromIntegral (LBS.length name) + Text.length anchorEnd
                   columnRange = ColumnRange{start = col1, end = col1 + markerLen - 1}
                   -- Advance the column number to match the remaining string `s2`
                   col2 = col1 + markerLen
-               in parseSomeMarker ((Anchor (TextL.toStrict name), columnRange) : anchors) refs s2 col2
+               in parseSomeMarker ((Anchor (toText name), columnRange) : anchors) refs s2 col2
             Just (Right (name, s2)) ->
-              let markerLen = Text.length refStart + fromIntegral (TextL.length name) + Text.length refEnd
+              let markerLen = Text.length refStart + fromIntegral (LBS.length name) + Text.length refEnd
                   columnRange = ColumnRange{start = col1, end = col1 + markerLen - 1}
                   col2 = col1 + markerLen
-               in parseSomeMarker anchors ((Reference (TextL.toStrict name), columnRange) : refs) s2 col2
+               in parseSomeMarker anchors ((Reference (toText name), columnRange) : refs) s2 col2
             Nothing
-              | TextL.null s1 -> (anchors, refs)
-              | otherwise -> parseSomeMarker anchors refs (TextL.drop 1 s1) (col1 + 1)
+              | LBS.null s1 -> (anchors, refs)
+              | otherwise -> parseSomeMarker anchors refs (LBS.drop 1 s1) (col1 + 1)
 
     parseAnchor = parseMarker anchorStart anchorEnd
     parseRef = parseMarker refStart refEnd
     parseMarker start end s0 = do
-      s1 <- TextL.stripPrefix (TextL.fromStrict start) s0
-      (name, s2) <- breakOn' (TextL.fromStrict end) s1
-      guard $ (not . TextL.null) name
+      s1 <- LBS.stripPrefix (toLBS start) s0
+      (name, s2) <- splitOnce (toLBS end) s1
+      guard $ (not . LBS.null) name
       pure (name, s2)
 
-    -- Same as breakOn, except returns Nothing if the delim isn't found, and
-    -- the snd string doesn't start with the delim.
-    breakOn' delim = traverse (TextL.stripPrefix delim) . TextL.breakOn delim
+{----- Utilities -----}
+
+{- | Split on the given delimiter
+
+>>> splitOnce "::" "a" == Nothing
+>>> splitOnce "::" "a::b::c" == Just ("a", "b::c")
+-}
+splitOnce :: LazyByteString -> LazyByteString -> Maybe (LazyByteString, LazyByteString)
+splitOnce delim =
+  let delimChar = LBS.head delim
+      go s =
+        case LBS.elemIndex delimChar s of
+          -- Delimiter not found
+          Nothing -> Nothing
+          Just i
+            -- Found delimiter
+            | Just s' <- LBS.stripPrefix delim (LBS.drop i s) -> Just (LBS.take i s, s')
+            -- False positive, try again
+            | otherwise -> go (LBS.drop (i + 1) s)
+   in go
